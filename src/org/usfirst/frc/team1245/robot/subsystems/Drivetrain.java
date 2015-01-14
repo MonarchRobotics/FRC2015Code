cb6ea4494a3f645118ead022428f1d2f5055b502
Boskin <boskin.erkocevic99@yahoo.com>
2015-01-14 14:43:15 -0700

    Added to drivetrain subsystem

--- src/org/usfirst/frc/team1245/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1245/robot/subsystems/Drivetrain.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team1245.robot.subsystems;
 
+import org.usfirst.frc.team1245.robot.commands.MecanumDrive;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -9,10 +13,22 @@ public class Drivetrain extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	
+	private RobotDrive robotDrivetrain;
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new MecanumDrive());
+    }
+    
+    public Drivetrain(int frontLeft, int rearLeft, int frontRight, int rearRight) {
+    	Talon tFrontLeft = new Talon(frontLeft);
+    	Talon tRearLeft = new Talon(rearLeft);
+    	Talon tFrontRight = new Talon(frontRight);
+    	Talon tRearRight = new Talon(rearRight);
+    	
+    	robotDrivetrain = new RobotDrive(tFrontLeft, tRearLeft, tFrontRight, tRearRight);
     }
 }
 
