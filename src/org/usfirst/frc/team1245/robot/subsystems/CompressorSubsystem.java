c3c8d0f6efea78afc6d5c95ecf5359c9882d16dc
leafypixiestix <justincai.lps@gmail.com>
2015-01-14 17:12:33 -0700

    Changed the Compressor subsystem's name to CompressorSubsystem to avoid naming conflicts

--- src/org/usfirst/frc/team1245/robot/subsystems/CompressorSubsystem.java
+++ src/org/usfirst/frc/team1245/robot/subsystems/CompressorSubsystem.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
-public class Compressor extends Subsystem {
+public class CompressorSubsystem extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
