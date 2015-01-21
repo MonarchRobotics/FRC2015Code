07c899f4b87175ceaac84f3fbaec7afaa06b3870
Boskin <boskin.erkocevic99@yahoo.com>
2015-01-20 18:14:05 -0700

    Completed pid drivetrain subsystem with abstraction

---
+++ src/org/usfirst/frc/team1245/robot/subsystems/SRXDrivetrain.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team1245.robot.subsystems;
+
+import org.usfirst.frc.team1245.robot.commands.MecanumDrivePID;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+
+/**
+ *
+ */
+public class SRXDrivetrain extends Drivetrain {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new MecanumDrivePID());
+    }
+    
+    public SRXDrivetrain(int frontLeft, int rearLeft, int frontRight, int rearRight) {
+    	tFrontLeft = new CANTalon(frontLeft);
+    	tRearLeft = new CANTalon(rearLeft);
+    	tFrontRight = new CANTalon(frontRight);
+    	tRearRight = new CANTalon(rearRight);
+    	
+    	robotDrive = new RobotDrive(tFrontLeft, tRearLeft, tFrontRight, tRearRight);
+    }
+}
+
