b706f0933ae74219d928fb214b13f986adda2f2e
SnowCrystalz <snowyleopard13@gmail.com>
2015-01-22 15:55:45 -0700

    Added Timer to Autonomous

--- src/org/usfirst/frc/team1245/robot/commands/DriveForward.java
+++ src/org/usfirst/frc/team1245/robot/commands/DriveForward.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1245.robot.commands;
 
 import org.usfirst.frc.team1245.robot.Robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -9,6 +10,8 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class DriveForward extends Command {
 
+	private Timer timer;
+	
     public DriveForward() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -17,6 +20,8 @@ public class DriveForward extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	timer = new Timer();
+    	timer.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -26,11 +31,16 @@ public class DriveForward extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	if(timer.get() >= 5){
+    		return true;
+    	}else{
+    		return false;
+    	}
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	timer.stop();
     }
 
     // Called when another command which requires one or more of the same
