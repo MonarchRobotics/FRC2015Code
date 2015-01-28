f8aedac80200852ad8b0e1784fb03571ed1493f1
Boskin <boskin.erkocevic99@yahoo.com>
2015-01-28 15:50:59 -0700

    Added some possibly useful speed constants to the Drivetrain class

--- src/org/usfirst/frc/team1245/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1245/robot/subsystems/Drivetrain.java
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team1245.robot.subsystems;
 
-import org.usfirst.frc.team1245.robot.commands.MecanumDrive;
-
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -20,6 +18,9 @@ public abstract class Drivetrain extends Subsystem {
 	protected SpeedController tRearLeft;
 	protected SpeedController tFrontRight;
 	protected SpeedController tRearRight;
+	
+	public static final double rpm = 5000;
+	public static final double rotationsPer10MS = 0.833;
 
     public abstract void initDefaultCommand();
     
